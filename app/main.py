8279ffb98d429e58139c1693830aab8d827576bd
YaGiNA <yutachihaya@gmail.com>
2017-04-14 18:11:56 +0900

    Complete to make function of getting hash

--- app/main.py
+++ app/main.py
@@ -1,4 +1,22 @@
 #!/usr/bin/env python3
+import requests
+from logging import getLogger, StreamHandler, DEBUG
+
+
+# Setting of logging function
+logger = getLogger(__name__)
+handler = StreamHandler()
+handler.setLevel(DEBUG)
+logger.setLevel(DEBUG)
+logger.addHandler(handler)
+
+
 def main(argv):
-  for v in argv:
-    print(v)
+    url = 'http://challenge-server.code-check.io/api/hash'
+
+    try:
+        query = {"q": argv[0]}
+    except NameError:
+        logger.debug("parameter is not exist.")
+    r = requests.get(url, params=query).json()
+    print(r["hash"])
